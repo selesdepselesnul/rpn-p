7fc76bb5ea6632639977f7418821fb0b1d51bfcc
selesdepselesnul <selesdepselesnul@gmail.com>
2015-11-26 05:33:20 +0700

    refactor

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from builtins import print
 import re
 
+
 class Stack(list):
     def push(self, value):
         self.append(value)
@@ -8,28 +9,28 @@ class Stack(list):
     def peek(self):
         return self[len(self) - 1]
 
-    def isEmpty(self):
+    def is_empty(self):
         return len(self) == 0
 
 
 class RpnParser(object):
-    def __init__(self, aritmethic_expr):
-        self.infix_list = aritmethic_expr
+    def __init__(self, arit_expr):
+        self.infix_list = arit_expr
         self.operator_stack = Stack()
         self.postfix_list = []
 
     def parse(self):
         for i in self.infix_list:
-            if self.isOperand(i):
+            if self.is_operand(i):
                 self.postfix_list.append(i)
-            elif self.isOperator(i):
-                if self.operator_stack.isEmpty():
+            elif self.is_operator(i):
+                if self.operator_stack.is_empty():
                     self.operator_stack.push(i)
                 elif i == '(':
                     self.operator_stack.push(i)
                 elif i == ')':
                     self.pop_operator_to_end_expr()
-                elif self.isGreater(i, self.operator_stack.peek()):
+                elif self.is_greater(i, self.operator_stack.peek()):
                     self.operator_stack.push(i)
                 else:
                     self.pop_until_greater(i)
@@ -42,7 +43,7 @@ class RpnParser(object):
         return str(self.postfix_list)
 
     def fill_postfix_stack(self):
-        while not self.operator_stack.isEmpty():
+        while not self.operator_stack.is_empty():
             self.postfix_list.append(self.operator_stack.pop())
 
     def pop_operator_to_end_expr(self):
@@ -51,12 +52,12 @@ class RpnParser(object):
         self.operator_stack.pop()
 
     def pop_until_greater(self, i):
-        while not self.operator_stack.isEmpty():
-            if self.isGreater(i, self.operator_stack.peek()):
+        while not self.operator_stack.is_empty():
+            if self.is_greater(i, self.operator_stack.peek()):
                 break
             self.postfix_list.append(self.operator_stack.pop())
 
-    def isGreater(self, op1, op2):
+    def is_greater(self, op1, op2):
         def is_lower_op(op):
             return op == '+' or op == '-'
 
@@ -67,10 +68,10 @@ class RpnParser(object):
                or ( (op1 == '^') and (is_mid_op(op2) or is_lower_op(op2)) ) \
                or ( is_mid_op(op1) and is_lower_op(op2) )
 
-    def isOperand(self, any_string):
+    def is_operand(self, any_string):
         return re.search('\\w', any_string)
 
-    def isOperator(self, any_string):
+    def is_operator(self, any_string):
         return re.search('[\+\-\*\/\^\(\)]', any_string)
 
 
